957189f2409714add4cee0ebb52c7d63d97382a8
Daniel Andres <dapalca@gmail.com>
2018-12-12 20:05:06 -0500

    Se crea el header y se despliega la lista de contactos inicialmente en un ScrollView. Se debe mejorar el estilo

--- App.js
+++ App.js
@@ -1,23 +1,10 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import { fetchUsers } from '@utils/api';
 import TabNavigator from '@navigation/TabNavigator'
 
 export default class App extends React.Component {
 
-  state = {
-    contacts: null,
-  }
-
-  componentDidMount() {
-    //this.getUsers()
-  }
-
-  getUsers = async () => {
-    const results = await fetchUsers()
-    this.setState({ contacts: results })
-  }
-
+  
   render() {
     return (
       <TabNavigator/>
